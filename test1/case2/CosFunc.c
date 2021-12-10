28076d00984d97aa9db0adcc0bb287c84a4bae25
vadim65060 <vadim65060@users.noreply.github.com>
2021-12-10 16:08:01 +0300

    update cos calculate in case2

--- test1/case2/CosFunc.c
+++ test1/case2/CosFunc.c
@@ -3,30 +3,8 @@
 //
 
 #include "CosFunc.h"
-#include <stdio.h>
 
-long long fact[20];
-double eps = 1e-5;
-
-void Factorial() {
-    fact[0] = 1;
-    fact[1] = 1;
-    for (int i = 2; i < 20; ++i) {
-        fact[i] = fact[i - 1] * i;
-    }
-}
-
-double SlowPow(double x, int n) {
-    double res = 1;
-    for (int i = 0; i < n; ++i) {
-        res *= x;
-    }
-    return res;
-}
-
-double CalcF(double x, int n) {
-    return SlowPow(-1, n) * SlowPow(x, n * 2) / fact[n * 2];
-}
+double eps = 1e-6;
 
 double fabs(double x) {
     if (x < 0)return -x;
@@ -38,17 +16,14 @@ double fabs(double x) {
  * return cos(x)
  */
 double cos(double x) {
-    if (fact[1] == 0)Factorial();
     int i = 0;
-    double newSum = CalcF(x, i);
+    double newSum = 1;
     double res = 0;
-    //printf("%lf\n",newSum);
     while (fabs(newSum) > eps) {
         res += newSum;
-        ++i;
-        newSum = CalcF(x, i);
-        //printf("res=%lf \n sum=%lf", res, newSum);
-        if (i >= 10)return -2;
+        i += 2;
+        newSum /= -((i - 1) * i / (x * x));
+        //printf("res=%lf \n sum=%lf\n", res, newSum);
     }
     return res;
 }
