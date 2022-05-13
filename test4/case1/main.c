7c3d55ff64d7c82ea8c623214c37d535fa16f8b0
vadim65060 <vadim65060@users.noreply.github.com>
2022-05-13 15:07:27 +0300

    add case1

---
+++ test4/case1/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define INF 9999999
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    double x, mx = -INF;
+    int mxI = -1, i = 0;
+    char c;
+    while (scanf("%lf%c", &x, &c) > 0) {
+        if (x > mx) {
+            mx = x;
+            mxI = i;
+        }
+        if (c == '\n') {
+            i++;
+            printf("%i\n", i);
+        }
+    }
+    printf("%i %lf", mxI, mx);
+}
